9ba309200208970698a122b1b156a04616bc0842
Nate <natethepcspecialist@gmail.com>
2014-06-27 11:44:33 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -18,12 +18,16 @@ $('body').append(logo.css({
 	position : 'fixed',
 	top : 28,
 	left : $('.logo').offset().left
-}));
+}).addClass('logocopy'));
 
 
 function changeBG (reverse, start) {
 	var start = start || 0;
+
 	if(start < 10){
 		n.animate({'opacity' : (reverse ? 0 : 1)}, 800);
+		if(!reverse)
+			$('.logocopy').fadeOut(800);
+		else $('.logocopy').fadeIn(800);
 	}
 }
