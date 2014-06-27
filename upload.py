4ed7ac5cb2a3a95b1cef2f0a989ead61dfc77046
Nate <natethepcspecialist@gmail.com>
2014-06-27 11:48:19 -0400

    update

--- upload.py
+++ upload.py
@@ -3,5 +3,3 @@ import os
 
 os.system("git add . && git commit -m 'update' && git push origin master")
 os.system("ssh mellopip@mellopipelines.com")
-os.system("cd public_html/garretscott/framecom")
-os.system("git pull")
